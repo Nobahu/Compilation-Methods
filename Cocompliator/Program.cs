af7264311602efb402a0a2f5c9625153dd7862eb
Nobahu <sasha.lyamburtsev@gmail.com>
2026-06-04 14:10:27 +0700

    Add Tests

--- Cocompliator/Program.cs
+++ Cocompliator/Program.cs
@@ -11,7 +11,7 @@ namespace Cocompliator
             try
             {
                 // Название нашего файла с формулой
-                string testFileName = "C:\\Users\\user\\source\\repos\\Cocompliator\\Cocompliator\\ErrorTest.txt";
+                string testFileName = "C:\\Users\\user\\source\\repos\\Cocompliator\\Cocompliator\\Tests\\diff_calc_test.txt";
                 string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, testFileName);
                 
                 // Чтение исходного кода из файла через ваш FileReader
